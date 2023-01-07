1750b3027673e1b18e92d4a7a6717ceaf546c9e5
Kayode Oluwasegun <segunkayode00@gmail.com>
2023-01-07 07:17:32 +0100

    added core feature to application

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
+const { Client } = require("@notionhq/client");
+const dotenv = require("dotenv");
 
-const sendTicketToNotionDatabase = () => {
-  return new Promise((res, rej) => {
+dotenv.config();
+
+const sendTicketToNotionDatabase = (text) => {
+  return new Promise(async (res, rej) => {
+    try {
+      const notion = new Client({
+        auth: process.env.NOTION_TOKEN,
+      });
+
+      const response = await notion.pages.create({
+        parent: { database_id: process.env.NOTION_DATABASE_ID },
+        properties: {
+          title: [
+            {
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      });
+
+      console.log("Entry successfully added");
+      res();
+    } catch (err) {
+      rej(err);
+    }
     // do random stuff for 3 seconds
     setTimeout(() => res("resolved"), 3000);
   });
@@ -24,7 +51,7 @@ app.whenReady().then(() => {
   ipcMain.on("ping", (event, text) => {
     console.log(text);
 
-    sendTicketToNotionDatabase().then((val) => {
+    sendTicketToNotionDatabase(text).then((val) => {
       console.log(val);
       app.quit();
     });
